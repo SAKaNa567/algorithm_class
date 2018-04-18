5ed810c154c94574039990a08c54a2987a2fa6ba
Naoto Kito <kito@soramitsu.co.jp>
2018-04-18 20:12:20 +0900

    version1

---
+++ heap_sort.py
@@ -0,0 +1,38 @@
+#Bottom up version
+def heapsort(random_list):
+    list_size = len(random_list) - 1
+    for i in range((list_size / 2), -1, -1):
+        MaxHeapify(random_list, i, list_size)
+
+    for i in range(list_size , 0, -1):
+        if random_list[0] > random_list[i]:
+            temp = random_list[0]
+            random_list[0] = random_list[i]
+            random_list[i] = temp
+            MaxHeapify(random_list, 0, i - 1)
+    return random_list
+
+def Left(i):
+    return i * 2 + 1
+
+def Right(i):
+    return i * 2 + 2
+
+def MaxHeapify(aList, root, bottom):
+    left = Left(root)
+    right = Right(root)
+
+    if left <= bottom and aList[left] > aList[root]:
+        max_child = left
+    else:
+        max_child = root
+
+    if right <= bottom and aList[right] > aList[max_child]:
+        max_child = right
+
+    if max_child != root:
+        aList[root], aList[max_child] = aList[max_child], aList[root]
+        MaxHeapify(aList, max_child, bottom)
+
+random_list = [3,99,0,11,23,9,75,52]
+print heapsort(random_list)
